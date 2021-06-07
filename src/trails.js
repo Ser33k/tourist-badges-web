059ab5e662710a833c6593d45afc5f5bda7e08d8
Mikolaj <mikus1001@gmail.com>
2021-06-07 21:06:22 +0200

    simple data json

---
+++ src/trails.js
@@ -0,0 +1,81 @@
+export const TRAILS = [
+    {
+        to: "Schronisko PTTK Hala Szrenicka (1195 m)",
+        entries: [
+            {
+                name: "z Przełęczy Szklarskiej (886 m)",
+                score: 10
+            },
+            {
+                name: "Przełęcz Szklarska (886 m) - przejście graniczne Jakuszyce",
+                score: 1
+            },
+            {
+                name: "z Wodospadu Kamieńczyk",
+                score: 6
+            },
+            {
+                name: "z Przełęczy Mokrej (940 m)",
+                score: 2
+            },
+            {
+                name: "z Końskich Łbów",
+                score: 1
+            },
+
+        ],
+        descending: [
+            {
+                name: "z Przełęczy Szklarskiej (886 m)",
+                score: 7
+            },
+            {
+                name: "Przełęcz Szklarska (886 m) - przejście graniczne Jakuszyce",
+                score: 1
+            },
+            {
+                name: "z Wodospadu Kamieńczyk",
+                score: 2
+            },
+            {
+                name: "z Przełęczy Mokrej (940 m)",
+                score: 3
+            },
+            {
+                name: "z Końskich Łbów",
+                score: 2
+            },
+        ]
+    },
+    {
+        to: "Szrenica (1362 m)",
+        entries: [
+            {
+                name: "ze Schroniska PTTK Hala Szrenicka (1195 m)",
+                score: 3
+            },
+            {
+                name: "z Końskich Łbów",
+                score: 2
+            },
+            {
+                name: "z Przełęczy Mokrej (940 m)",
+                score: 2
+            }
+        ],
+        descending: [
+            {
+                name: "ze Schroniska PTTK Hala Szrenicka (1195 m)",
+                score: 1
+            },
+            {
+                name: "z Końskich Łbów",
+                score: 1
+            },
+            {
+                name: "z Przełęczy Mokrej (940 m)",
+                score: 1
+            }
+        ]
+    }
+]
