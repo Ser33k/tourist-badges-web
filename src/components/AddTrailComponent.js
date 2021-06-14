c57d71f6c7e8cdc8265e8996d3e69c38297f9a87
Mikolaj <mikus1001@gmail.com>
2021-06-14 12:33:42 +0200

    addTrail component styled and working well

--- src/components/AddTrailComponent.js
+++ src/components/AddTrailComponent.js
@@ -1,35 +1,73 @@
 import React, {useState} from 'react';
-import Paper from '@material-ui/core/Paper';
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import {makeStyles, withStyles} from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import Alert from '@material-ui/lab/Alert';
+
+import up from './images/up.png'
+import down from './images/down.png'
+import map from './images/map.png'
 
 import {TRAILS as trails} from "../trails";
-import InputBase from "@material-ui/core/InputBase";
+import {Button} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
     form: {
         width: '100%'
     },
-    input:{
+    input: {
+        width: '100%',
         "& .MuiAutocomplete-endAdornment": {
             display: "none"
+        },
+
+    },
+    firstInput: {
+        "& > div": {
+            backgroundColor: "white",
+        }
+    },
+    row: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+        "& img": {
+            height: 'auto',
+            width: '10%'
         }
     },
     disabled: {
-        backgroundColor: "#ddd"
-    }
+        backgroundColor: "#eff"
+    },
+    equation: {
+        display: "flex",
+        fontSize: "40px",
+        justifyContent: "space-around",
+        marginTop: "40px"
+    },
+    button: {
+        background: "#A3D3FF",
+        fontSize: "30px",
+        marginTop: "40px",
+        color: "#223E57",
+        "&:hover": {
+            backgroundColor: "#223E57",
+            color: "#A3D3FF"
+        }
+    },
 }))
 
-const AddTrailComponent = () => {
+const AddTrailComponent = (props) => {
     const classes = useStyles();
 
     const [trail, setTrail] = useState('');
     const [from, setFrom] = useState('');
     const [to, setTo] = useState('');
+
 
     const handleChangeTrail = (event) => {
         setTrail(event.target.value);
@@ -37,128 +75,101 @@ const AddTrailComponent = () => {
         setTo('')
     }
 
+
     const handleChangeFrom = (event) => {
-        const value = trails.filter(t => t.to === trail)[0].entries.filter(t => event.target.value === t.name)[0];
+        const value = trails.filter(t => t.to === trail)[0].entries.filter(t => event.target.value.includes(t.name))[0];
         setFrom(value);
     }
 
     const handleChangeTo = (event) => {
-        const value = trails.filter(t => t.to === trail)[0].descending.filter(t => event.target.value === t.name)[0];
+        const value = trails.filter(t => t.to === trail)[0].descending.filter(t => event.target.value.includes(t.name))[0];
         console.log(value)
         setTo(value);
     }
 
-    const isTrailChoose = trails.some(t => t.to === trail);
-    console.log(isTrailChoose)
+
+    const addTrail = (e) => {
+        e.preventDefault();
+        props.handleAddTrail(trail, from, to);
+
+        setTo(null)
+        setFrom(null)
+        setTrail({})
+
+    }
+    const isTrailSelected = trails.some(t => t.to === trail);
 
     return (
-        <Paper className={'content'}>
-            <p>{from && from.score}</p>
-            <p>{to && to.score}</p>
-            <form className={classes.form}>
-                <Autocomplete
-                    id="free-solo-demo"
-                    options={trails.map((option) => option.to)}
-                    className={classes.input}
-                    fullWidth
-                    disableClearable
-                    disableListWrap
-                    renderInput={(params) => (
-                        <TextField {...params} onSelect={handleChangeTrail} onChange={handleChangeTrail} label="Trail" margin="normal" fullWidth variant="outlined" />
-                    )}
-                />
-                <Autocomplete
-                    id="free-solo-demo"
-                    options={isTrailChoose ? trails.filter(t => t.to === trail)[0].entries.map(trail => trail.name) : undefined}
+        <>
+            <div className={'content'}>
+                <div className={classes.img}>
+                    <img src={map} alt=""/>
+                </div>
+                <form className={classes.form} onSubmit={addTrail}>
+                    <Autocomplete
+                        id="free-solo-demo"
+                        options={trails.map((option) => option.to)}
+                        className={`${classes.input} ${classes.firstInput}`}
+                        disableClearable
+                        disableListWrap
 
-                    className={classes.input}
-                    fullWidth
-                    disableClearable
-                    disabled={!isTrailChoose}
-                    renderInput={(params) => (
-                        <TextField  {...params} className={!isTrailChoose ? classes.disabled : undefined} onSelect={handleChangeFrom} onChange={handleChangeFrom} label="From" margin="normal" fullWidth variant="outlined" />
-                    )}
-                />
-                <Autocomplete
-                    id="free-solo-demo"
-                    // options={trails.map((option) => option.to)}
-                    options={isTrailChoose ? trails.filter(t => t.to === trail)[0].descending.map(trail => trail.name) : undefined}
-                    fullWidth
-                    disableClearable
-                    disabled={!isTrailChoose}
-                    className={classes.input}
-                    renderInput={(params) => (
-                        <TextField {...params} className={!isTrailChoose ? classes.disabled : undefined} onSelect={handleChangeTo} onChange={handleChangeTo} label="To" margin="normal" fullWidth variant="outlined" />
-                    )}
-                />
+                        renderInput={(params) => (
+                            <TextField {...params} onSelect={handleChangeTrail} onChange={handleChangeTrail}
+                                       label="Trail" value={trail} margin="normal" fullWidth variant="outlined"/>
+                        )}
+                    />
+                    <Autocomplete
+                        id="free-solo-demo"
+                        options={isTrailSelected ? trails.filter(t => t.to === trail)[0].entries.map(trail => `${trail.name} - ${trail.score} pt.`) : []}
+                        className={classes.input}
+                        disableClearable
+                        disabled={!isTrailSelected}
+                        renderInput={(params) => (
+                            <TextField  {...params} value={from && from.name} style={!isTrailSelected ?
+                                {backgroundColor: "#f1f0f0"} : {backgroundColor: "white"}} onSelect={handleChangeFrom}
+                                        onChange={handleChangeFrom}  label="From" margin="normal" variant="outlined"/>
+                        )}
+                    />
+                    <Autocomplete
+                        id="free-solo-demo"
+                        // options={trails.map((option) => option.to)}
+                        options={isTrailSelected ? trails.filter(t => t.to === trail)[0].descending.map(trail => `${trail.name} - ${trail.score} pt.`) : []}
+                        disableClearable
+                        disabled={!isTrailSelected}
+                        className={classes.input}
+                        renderInput={(params) => (
+                            <TextField {...params} style={!isTrailSelected ?
+                                {backgroundColor: "#f1f0f0"} : {backgroundColor: "white"}} onSelect={handleChangeTo}
+                                       onChange={handleChangeTo}  label="To" margin="normal" fullWidth
+                                       variant="outlined"/>
+                        )}
+                    />
+
+                    {from && to &&
+                    <div className={classes.equation}>
+                        <div className={classes.img}>
+                            <img src={up} alt=""/>
+                        </div>
+                        +
+                        <div className={classes.img}>
+                            <img src={down} alt=""/>
+                        </div>
+                        =
+                        <div>
+                            <strong>{from.score + to.score}</strong>
+                        </div>
+                    </div>
+                    }
+
+                    {props.success && <Alert severity="success">Trail added successfully!</Alert>}
+                    <Button type={'submit'} variant={"contained"} color={"primary"} className={classes.button}>Add
+                        Trail</Button>
+
+                </form>
 
 
-                {/*<InputLabel id="demo-customized-select-label">Trail</InputLabel>*/}
-                {/*<Select*/}
-                {/*    labelId="demo-customized-select-label"*/}
-                {/*    id="demo-customized-select"*/}
-                {/*    value={trail}*/}
-                {/*    onChange={handleChangeTrail}*/}
-                {/*    input={<BootstrapInput/>}*/}
-                {/*>*/}
-                {/*    <MenuItem value="">*/}
-                {/*        <em>None</em>*/}
-                {/*    </MenuItem>*/}
-                {/*    {trails.map(trail => <MenuItem value={trail.to}>{trail.to}</MenuItem>)}*/}
-                {/*    /!*<MenuItem value={10}>Ten</MenuItem>*!/*/}
-                {/*    /!*<MenuItem value={20}>Twenty</MenuItem>*!/*/}
-                {/*    /!*<MenuItem value={30}>Thirty</MenuItem>*!/*/}
-                {/*</Select>*/}
-
-                {/*{trail !== '' ?*/}
-                {/*    (*/}
-                {/*        <>*/}
-                {/*            <InputLabel id="demo-customized-select-label">From</InputLabel>*/}
-                {/*            <Select*/}
-                {/*                labelId="demo-customized-select-label"*/}
-                {/*                id="demo-customized-select1"*/}
-                {/*                value={from}*/}
-                {/*                onChange={handleChangeFrom}*/}
-                {/*                input={<BootstrapInput/>}*/}
-
-                {/*            >*/}
-                {/*                <MenuItem value="">*/}
-                {/*                    <em>None</em>*/}
-                {/*                </MenuItem>*/}
-                {/*                {trails.filter(t => t.to === trail)[0].entries.map(trail => <MenuItem*/}
-                {/*                    value={trail}>{trail.name}</MenuItem>)}*/}
-                {/*            </Select>*/}
-                {/*            <p>{from.score}</p>*/}
-
-                {/*            <InputLabel id="demo-customized-select-label">To</InputLabel>*/}
-                {/*            <Select*/}
-                {/*                labelId="demo-customized-select-label"*/}
-                {/*                id="demo-customized-select1"*/}
-                {/*                value={to}*/}
-                {/*                onChange={handleChangeTo}*/}
-                {/*                input={<BootstrapInput/>}*/}
-
-                {/*            >*/}
-                {/*                <MenuItem value="">*/}
-                {/*                    <em>None</em>*/}
-                {/*                </MenuItem>*/}
-                {/*                {trails.filter(t => t.to === trail)[0].descending.map(trail => <MenuItem*/}
-                {/*                    value={trail}>{trail.name}</MenuItem>)}*/}
-                {/*                /!*<MenuItem value={10}>Ten</MenuItem>*!/*/}
-                {/*                /!*<MenuItem value={20}>Twenty</MenuItem>*!/*/}
-                {/*                /!*<MenuItem value={30}>Thirty</MenuItem>*!/*/}
-                {/*            </Select>*/}
-                {/*            <p>{to.score}</p>*/}
-                {/*        </>*/}
-                {/*    )*/}
-
-                {/*    :*/}
-
-                {/*    null*/}
-                {/*}*/}
-
-            </form>
-        </Paper>
+            </div>
+        </>
     );
 };
 
