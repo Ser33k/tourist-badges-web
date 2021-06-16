30b5035da445c62e54b50f06a5ac6456082584a9
Mikolaj <mikus1001@gmail.com>
2021-06-16 08:56:02 +0200

    info modal in badgesComponent

--- src/components/BadgesComponent.js
+++ src/components/BadgesComponent.js
@@ -5,7 +5,11 @@ import brazowa from './images/brazowa.png';
 import srebrna from './images/srebrna.png';
 import zlota from './images/zlota.png';
 import {makeStyles} from "@material-ui/core/styles";
-import {Typography} from "@material-ui/core";
+import {Button, Typography} from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import Modal from '@material-ui/core/Modal';
+import instruction from './images/instruction.JPG'
 
 const useStyles = makeStyles((theme) => ({
     badges: {
@@ -14,7 +18,8 @@ const useStyles = makeStyles((theme) => ({
         marginTop: '50px',
         marginLeft: 'auto',
         marginRight: 'auto',
-        padding: "10px"
+        padding: "10px",
+        position: "relative",
     },
     images: {
         display: "flex",
@@ -30,19 +35,74 @@ const useStyles = makeStyles((theme) => ({
         paddingBottom: '5px',
         display: "inline-block",
         color: '#72b4fc'
-
+    },
+    infoButton: {
+        position: "absolute",
+        right: "0px",
+        top: "0px",
+        color: "black"
+    },
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: "3px 3px 3px 5px #fff",
+        padding: theme.spacing(3, 4),
+        outline: "none",
+        textAlign: "center"
+    },
+    modalButton: {
+        backgroundColor: "black",
+        color: "#fff",
+        fontSize: 20,
+        // padding: "10px 20px",
+        margin: "5px 0 0 0",
+        border: "1px solid black",
+        "&:hover": {
+            backgroundColor: "white",
+            color: "black"
+        }
+    },
+    instructionImg: {
+        maxWidth: "100%",
+        height: "auto"
     }
 
 }));
 
+function getModalStyle() {
+    const top = 50
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
 
 const BadgesComponent = (props) => {
     const classes = useStyles();
+
+    const [openModal, setOpenModal] = useState(true);
+    const [modalStyle] = useState(getModalStyle);
 
     const [popularOpacity, setPopularOpacity] = useState(true);
     const [brownOpacity, setBrownOpacity] = useState(true);
     const [silverOpacity, setSilverOpacity] = useState(true);
     const [goldOpacity, setGoldOpacity] = useState(true);
+
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+
+    const body = (
+        <div style={modalStyle} className={classes.paper}>
+            <img className={classes.instructionImg} src={instruction} alt="instruction"/>
+            <Button className={classes.modalButton} onClick={handleCloseModal}>OK!</Button>
+        </div>
+    );
 
     const setAllBadges = () => {
         setPopularOpacity(false);
@@ -97,7 +157,11 @@ const BadgesComponent = (props) => {
         setBadgesOpacity();
     }, [props.points])
     return (
+        <>
         <Paper className={classes.badges}>
+            <IconButton onClick={() => setOpenModal(true)} className={classes.infoButton}>
+                <InfoOutlinedIcon />
+            </IconButton>
             <Typography className={classes.header} variant={'h3'}>YOUR BADGES</Typography>
             <div className={classes.images}>
             <img id={'popularna'} className={popularOpacity ? classes.opacity : undefined} src={popularna} alt=""/>
@@ -106,7 +170,18 @@ const BadgesComponent = (props) => {
             <img id={'zlota'} className={goldOpacity ? classes.opacity : undefined} src={zlota} alt=""/>
 
             </div>
+
+
         </Paper>
+            <Modal
+                open={openModal}
+                onClose={handleCloseModal}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                {body}
+            </Modal>
+            </>
     );
 };
 
